a852d4c7fd68422e0ad5d34ec060e446101adc1d
Eddie Melo <heriberto@melo.nyc>
2018-05-02 22:38:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function findMatching(drivers, string){
   var new_drivers = [];
   for(const driver of drivers){
-    if (driver.tolowercase() == string.tolowercase()){
+    if (driver == string){
       new_drivers.push(driver);
     }
   }
